e38cafc370a0d8ba750af5276e115d7782c07109
Nishint0001 <nishintgoyal0001@gmail.com>
2023-06-30 12:36:22 +0530

    first commit

--- application.py
+++ application.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
+application = Flask(__name__)
+app=application
 
 ##IMPORT RIDGE REGRESSOR MODEL AND STANDARD SCALER PICKLE
 ridge_model=pickle.load(open("models/ridge.pkl","rb"))
